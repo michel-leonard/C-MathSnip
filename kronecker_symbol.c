8f316feb59950b6f202d66019b7fd1e2a2a74390
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-04-28 22:55:16 +0200

    Create kronecker_symbol.c

---
+++ kronecker_symbol.c
@@ -0,0 +1,40 @@
+//  This function returns the Kronecker symbol of A and B.
+int kronecker(unsigned a, unsigned b) {
+    static const int tab[8] = {0, 1, 0, -1, 0, -1, 0, 1};
+    unsigned c;
+    int res = (int) (a | b);
+    if (a && b)
+        if (res & 1) {
+            for (c = 0; !(b & 1); ++c, b >>= 1);
+            // for for then ... res *= Jacobi symbol A/B ]-[ when b is odd the Jacobi symbol and Kronecker symbol are identical.
+            for (res = c & 1 ? tab[a & 7] : 1; a; c & 1 ? res *= tab[b & 7] : 0, a & b & 2 ? res = -res : 0, c = b % a, b = a, a = c)
+                for (c = 0; !(a & 1); ++c, a >>= 1);
+            res = b == 1 ? res : 0;
+        } else res = 0;
+    else res = res == 1;
+    return res;
+}
+
+
+#include <assert.h>
+int main() {
+    assert(kronecker(0, 0) == 0);
+    assert(kronecker(2, 0) == 0);
+    assert(kronecker(3, 0) == 0);
+    assert(kronecker(0, 1) == 1);
+    assert(kronecker(1, 1) == 1);
+    assert(kronecker(2, 1) == 1);
+    assert(kronecker(2, 3) == -1);
+    assert(kronecker(3, 5) == -1);
+    assert(kronecker(5, 2) == -1);
+
+    assert(kronecker(900, 900) == 0);
+    assert(kronecker(902, 902) == 0);
+    assert(kronecker(903, 903) == 0);
+    assert(kronecker(901, 902) == 1);
+    assert(kronecker(902, 907) == 1);
+    assert(kronecker(903, 908) == 1);
+    assert(kronecker(901, 903) == -1);
+    assert(kronecker(902, 903) == -1);
+    assert(kronecker(903, 907) == -1);
+}
