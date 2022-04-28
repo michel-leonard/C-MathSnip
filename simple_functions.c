a41210242c67c8a9f6606e62a29e3e44983d7ae8
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-04-28 17:45:23 +0200

    Update simple_functions.c

--- simple_functions.c
+++ simple_functions.c
@@ -13,9 +13,9 @@ unsigned square_root(unsigned n){
 
 // return a number that, when multiplied by itself nth times, makes N.
 unsigned nth_root(const unsigned n, const unsigned nth) {
-    unsigned a = n, c, d, r = nth ? n + (n > 1) : n == 1 ;
-    for (; a < r; c = a + (nth - 1) * r, a = c / nth)
-        for (r = a, a = n, d = nth - 1; d && (a /= r); --d);
+    unsigned a = n, b, c, r = nth ? n + (n > 1) : n == 1 ;
+    for (; a < r; b = a + (nth - 1) * r, a = b / nth)
+        for (r = a, a = n, c = nth - 1; c && (a /= r); --c);
     return r;
 }
 
