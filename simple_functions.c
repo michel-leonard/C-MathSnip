dbdb7b6245da1f47aa70ec9cf25462eb8b078c19
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-04-28 14:57:33 +0200

    Update simple_functions.c

--- simple_functions.c
+++ simple_functions.c
@@ -1,6 +1,6 @@
 // return the largest number that can be used to divide the two numbers.
 unsigned gcd(unsigned a, unsigned b){
-    if (a && b) for(;(a %= b) && (b %= a););
+    if (b) for(;(a %= b) && (b %= a););
     return a | b;
 }
 
