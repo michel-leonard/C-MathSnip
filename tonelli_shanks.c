4a05b3338921d09dd45d4ca7eb5945b777f39f45
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-04-28 13:59:21 +0200

    Update tonelli_shanks.c

--- tonelli_shanks.c
+++ tonelli_shanks.c
@@ -1,4 +1,4 @@
-// return (lhs * rhs) % mod
+// return (a * b) % mod, avoiding overflow errors while doing modular multiplication.
 static unsigned multiplication_modulo(unsigned a, unsigned b, const unsigned mod) {
     unsigned res = 0, tmp;
     for (b %= mod; a; a & 1 ? b >= mod - res ? res -= mod : 0, res += b : 0, a >>= 1, (tmp = b) >= mod - b ? tmp -= mod : 0, b += tmp);
@@ -8,11 +8,7 @@ static unsigned multiplication_modulo(unsigned a, unsigned b, const unsigned mod
 // return (n ^ exp) % mod
 static unsigned mod_pow(unsigned n, unsigned exp, const unsigned mod) {
     unsigned res = 1;
-    for (n %= mod; exp; exp >>= 1) {
-        if (exp & 1)
-            res = multiplication_modulo(res, n, mod);
-        n = multiplication_modulo(n, n, mod);
-    }
+    for (n %= mod; exp; exp & 1 ? res = multiplication_modulo(res, n, mod) : 0, n = multiplication_modulo(n, n, mod), exp >>= 1);
     return res;
 }
 
