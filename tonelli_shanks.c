6a0b936ac344593695ece1a1e79dc70760d2d689
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-04-28 04:07:59 +0200

    Update tonelli_shanks.c

--- tonelli_shanks.c
+++ tonelli_shanks.c
@@ -1,18 +1,25 @@
-// return (n ^ exp) % mod.
-unsigned mod_pow(unsigned n, unsigned exp, const unsigned mod) {
-    unsigned a, b, res = 1;
+// return (lhs * rhs) % mod
+static unsigned multiplication_modulo(unsigned lhs, unsigned rhs, const unsigned mod) {
+    unsigned res = 0;
+    for (lhs %= mod, rhs%= mod; rhs; (rhs & 1) ? (res = (res + lhs) % mod) : 0, lhs = (lhs << 1) % mod, rhs >>= 1);
+    return res;
+}
+
+// return (n ^ exp) % mod
+static unsigned mod_pow(unsigned n, unsigned exp, const unsigned mod) {
+    unsigned res = 1;
     for (n %= mod; exp; exp >>= 1) {
-            if (exp & 1)
-                for (a = res, b = n, res = 0; b; b & 1 ? res = (res + a) % mod : 0, a = a << 1 % mod, b >>= 1); // res = (res * x) % n ;
-            for (a = n, b = n, n = 0; b; b & 1 ? n = (n + a) % mod : 0, a = a << 1 % mod, b >>= 1); // x = (x * x) % n ;
-        }
+        if (exp & 1)
+            res = multiplication_modulo(res, n, mod);
+        n = multiplication_modulo(n, n, mod);
+    }
     return res;
 }
 
 // return root such that (root * root) % mod congruent to n % mod.
-// return mod if n is a quadratic non-residue modulo mod.
-unsigned tonelli_shanks(unsigned n, const unsigned mod) {
-    unsigned a = mod - 1, b = a, c = 0, d = 2, e, f, r = mod; // r = return value if no solution to the congruence exists.
+// return mod if n is a quadratic non-residue modulo mod, no solution to the congruence exists.
+static unsigned tonelli_shanks(unsigned n, const unsigned mod) {
+    unsigned a = mod - 1, b = a, c = 0, d = 2, e, f, r = mod;
     if (mod_pow(n, b >> 1, mod) == 1) {
         for (; !(a & 1); ++c, a >>= 1);
         if (c == 1)
@@ -21,12 +28,12 @@ unsigned tonelli_shanks(unsigned n, const unsigned mod) {
             for (; b != mod_pow(d, b >> 1, mod); ++d);
             for (b = mod_pow(d, a, mod), r = mod_pow(n, (a + 1) >> 1, mod), e = mod_pow(n, a, mod), f = c; e != 1; f = a) {
                 for (a = 0, c = e, --f; c != 1 && a < f; ++a)
-                    for (d = c, n = c, c = 0; n; n & 1 ? c = (c + d) % mod : 0, d = d << 1 % mod, n >>= 1); // c = (c * c) % mod ;
+                    c = multiplication_modulo(c, c, mod);
                 for (d = b, n = f - a; n--;)
-                    for (c = d, f = d, d = 0; f; f & 1 ? d = (d + c) % mod : 0, c = c << 1 % mod, f >>= 1); // d = (d * d) % mod ;
-                for (c = r, f = d, r = 0; f; f & 1 ? r = (r + c) % mod : 0, c = c << 1 % mod, f >>= 1); // r = (r * d) % mod ;
-                for (c = d, f = d, b = 0; f; f & 1 ? b = (b + c) % mod : 0, c = c << 1 % mod, f >>= 1); // b = (d * d) % mod ;
-                for (c = e, f = b, e = 0; f; f & 1 ? e = (e + c) % mod : 0, c = c << 1 % mod, f >>= 1); // e = (e * b) % mod ;
+                    d = multiplication_modulo(d, d, mod);
+                r = multiplication_modulo(r, d, mod);
+                b = multiplication_modulo(d, d, mod);
+                e = multiplication_modulo(e, b, mod);
             }
         }
     }
@@ -40,8 +47,8 @@ int main() {
     n = 27875, mod = 26371, root = tonelli_shanks(n, mod);
     assert(root == 14320); // 14320 * 14320  mod  26371 = 1504     and   1504 =    27875 mod 26371
 
-    n = 2675, mod = 7757, root = tonelli_shanks(n, mod);
-    assert(root == 2034); // 2034 * 2034   mod   7757 = 2675     and   2675 =     2675 mod 7757
+    n = 1111111111, mod = 1111111121, root = tonelli_shanks(n, mod);
+    assert(root == 88664850);
 
     n = 5258, mod = 3851, root = tonelli_shanks(n, mod);
     assert(root == 3851); // no solution to the congruence exists.
