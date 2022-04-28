c539097a7e8a9f17c583d1342f8a32ac3e38d3f2
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-04-28 14:35:53 +0200

    Update simple_functions.c

--- simple_functions.c
+++ simple_functions.c
@@ -7,7 +7,7 @@ unsigned gcd(unsigned a, unsigned b){
 // return the number that was multiplied by itself to reach N.
 unsigned square_root(unsigned n){
     unsigned a = n > 0, b;
-    if (n > 1) for (a = n >> 1, b = (a + n / a) >> 1; b < a; a = b, b = (a + n / a) >> 1);
+    if (n > 3) for (a = n >> 1, b = (a + n / a) >> 1; b < a; a = b, b = (a + n / a) >> 1);
     return a ;
 }
 
@@ -19,8 +19,8 @@ unsigned int absolute(int n){
 }
 
 #include <stdlib.h>
-unsigned short random(){
-    unsigned short n = 0 ;
+unsigned random(){
+    unsigned n = 0 ;
     for (size_t i = 0; i < sizeof(n); ((char*)&n)[i++] = rand());
     return n ;
 }
