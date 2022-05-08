0ddec4558addc367826e2f6b7c83877e72254951
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-05-08 23:48:00 +0200

    Update simple_functions.c

--- simple_functions.c
+++ simple_functions.c
@@ -39,7 +39,7 @@ unsigned cube_root(unsigned n){
     unsigned a = 0, b;
     for (int c = sizeof(unsigned) * CHAR_BIT / 3 * 3 ; c >= 0; c -= 3) {
         a <<= 1;
-        b = 3 * a * (a + 1) + 1;
+        b = a + (a << 1), b = b * a + b + 1 ;
         if (n >> c >= b)
             n -= b << c, ++a;
     }
