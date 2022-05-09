1023618df39693bee33c84139ac23187359382b9
michel-leonard <101180436+michel-leonard@users.noreply.github.com>
2022-05-09 10:53:55 +0200

    Create sqrt.c

---
+++ simple_functions/sqrt.c
@@ -0,0 +1,19 @@
+// return the number that was multiplied by itself to reach N.
+unsigned square_root_1(const unsigned num) {
+    unsigned a, b, c, d;
+    for (b = a = num, c = 1; a >>= 1; ++c);
+    for (c = 1 << (c & -2); c; c >>= 2) {
+        d = a + c;
+        a >>= 1;
+        if (b >= d)
+            b -= d, a += c;
+    }
+    return a;
+}
+
+// return the number that was multiplied by itself to reach N.
+unsigned square_root_2(unsigned n){
+    unsigned a = n > 0, b;
+    if (n > 3) for (a = n >> 1, b = (a + n / a) >> 1; b < a; a = b, b = (a + n / a) >> 1);
+    return a ;
+}
